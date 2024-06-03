e98fdb8486f73d98ed16049f439ad89ad95d94fe
Alfajri <alfajri607@gmail.com>
2024-06-04 06:43:14 +0700

    first init

---
+++ create-component/src/App.js
@@ -0,0 +1,12 @@
+import {Profile} from "./FirstComponent";
+
+function App() {
+    return (
+        <section>
+            <h1>Amazing scientists</h1>
+            <Profile/>
+        </section>
+    );
+}
+
+export default App;
